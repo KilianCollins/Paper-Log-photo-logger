a69962b6a8f3774905a304cbe7d969c0ebf30a09
Kilian Collins <134647067+KilianCollins@users.noreply.github.com>
2026-08-13 15:33:18 -0500

    Update config.js
    addded a drive api

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // authenticate it) and is safe to commit. See README.md "Human setup steps" for
 // how to obtain it.
 var CONFIG = {
-  GOOGLE_CLIENT_ID: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '534125974990-cqgr30g2v39dn1glvv2u4f6fr7el269j.apps.googleusercontent.com', // created 3:30pm 8-13-26 by kilian
   DRIVE_SCOPE: 'https://www.googleapis.com/auth/drive',
   ROOT_FOLDER_NAME: 'Well Photos'
 };
